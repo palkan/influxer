7c7cc3d0fcba80042307b2d0dddaf5fa8e5455f6
palkan <dementiev.vm@gmail.com>
2015-01-19 18:14:54 +0400

    ...

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -7,6 +7,7 @@ require "influxer"
 
 module Dummy
   class Application < Rails::Application
+    config.cache_store = :memory_store
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
