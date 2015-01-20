89b8695dd74c84501d3511960d8691253ec5441a
palkan <dementiev.vm@gmail.com>
2015-01-20 14:32:49 +0300

    add anyway_config

--- lib/influxer/config.rb
+++ lib/influxer/config.rb
@@ -1,50 +1,26 @@
+require 'anyway'
+
 module Influxer
-  class Config
-    attr_accessor :database,
-                  :host,
-                  :port,
-                  :username,
-                  :password,
-                  :use_ssl,
-                  :async,
-                  :cache,
-                  :retry,
-                  :time_precision,
-                  :initial_delay,
-                  :max_delay,
-                  :read_timeout,
-                  :write_timeout
+  class Config < Anyway::Config
+    config_name :influxdb
 
-    def initialize
-      @database = 'db'
-      @host = 'localhost'
-      @port = 8083
-      @use_ssl = false
-      @async = true
-      @cache = false
-      @retry = false
-      @time_precision = 's'
-      @max_delay = 30
-      @initial_delay = 0.01
-      @read_timeout = 30
-      @write_timeout = 5 
+    attr_config database: 'db', 
+                host: 'localhost',
+                port: 8083,
+                username: 'root',
+                password: 'root',
+                use_ssl: false,
+                async: true,
+                cache: false,
+                retry: false,
+                time_precision: 's',
+                initial_delay: 0.01,
+                max_delay: 30,
+                read_timeout: 30,
+                write_timeout: 5
 
-      config_path = Rails.root.join("config","influxdb.yml")
-      
-      config = {}
-
-      if File.file? config_path
-        config = YAML.load_file(config_path)[Rails.env] || {}
-      end
-
-      unless Rails.application.try(:secrets).nil?
-        config.merge! Rails.application.secrets.influxdb
-      end
-
-      config.each do |key, val| 
-        self.send("#{key}=",val)
-      end
-
+    def load
+      super
       # we want pass @cache value as options to cache store, so we want it to be a Hash
       if @cache == true
         @cache = {}
